861f38ab9b846deb82d19d4fee0cfbc40f6753a1
prisca2920 <prisca2920@gmail.com>
2024-09-17 15:10:41 +0300

    how to fetch goods from the db and calculate them

---
+++ haven/cart/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
+
+
+
+class CartItem(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.product:
+            return f'{self.quantity} x {self.product.name}'
+        else:
+            return 'Invalid Cart Item'
