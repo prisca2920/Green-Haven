10f2a551ff4a883bdb1ce03d5c81070abb284642
prisca2920 <prisca2920@gmail.com>
2024-09-17 15:11:33 +0300

    the paths to the urls in the main app

---
+++ haven/cart/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from . import views
+from django.contrib import admin
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart_count/', views.cart_count, name='cart_count'),
+    path('details/', views.details, name='details'),
+    path('contact/', views.contact, name='contact'),
+    path('cart/', views.cart, name='cart'),
+    path('faq/', views.faq_view, name='faq'),
+    path('admin/', admin.site.urls),
+    path('googlelogin/', include('googlelogin.urls')),
+]
